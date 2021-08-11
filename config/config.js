e28ddf23d91f10b72652c44a9a6ce191fb221776
luckydh <sajfrontdev@gmail.com>
2021-08-11 22:24:38 +0545

    mongodb cluster change

--- config/config.js
+++ config/config.js
@@ -4,6 +4,6 @@ const config = {
   port: process.env.PORT || 3000,
   mongoUri:
     process.env.MONGODB_URI ||
-    "mongodb+srv://prize:reward@cluster0.xg5z4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    "mongodb+srv://prize:reward@cluster0.rwdxc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
 };
 module.exports = { config };
